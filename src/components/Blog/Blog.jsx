efc901e89d74e25454c86e7cafec45d238a97ec3
TahsinAhammed16 <tahsin.ahammed16@gmail.com>
2023-09-14 00:18:03 +0600

    local images added, font added, layout with responsive header, props type issue

---
+++ src/components/Blog/Blog.jsx
@@ -0,0 +1,11 @@
+import PropTypes from "prop-types";
+
+const Blog = ({ blog }) => {
+  return <div></div>;
+};
+
+export default Blog;
+
+Blog.propTypes={
+    blog: PropTypes.object.isRequired
+}
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -11,8 +11,9 @@ const Blogs = () => {
   }, []);
 
   return (
-    <div>
-      <h1></h1>
+    <div className="md:w-2/3">
+      <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+      
     </div>
   );
 };
